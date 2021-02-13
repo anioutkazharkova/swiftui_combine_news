c1f8c228287db5947b58a5763272624eb653b8c4
azharkova <azharkova@usetech.ru>
2021-02-13 13:26:21 +0700

    Init

---
+++ news_swiftui/news_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  news_swiftui
+//
+//  Created by Anna Zharkova on 13.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ news_swiftui/news_swiftui/news_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  news_swiftuiApp.swift
+//  news_swiftui
+//
+//  Created by Anna Zharkova on 13.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct news_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
